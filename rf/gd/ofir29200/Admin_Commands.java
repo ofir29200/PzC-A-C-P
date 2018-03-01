c6f6208cb7cc315a711b809ac999f37cd481b812
ofir29200 <ofir29200@gmail.com>
2018-03-02 01:02:07 +0200

    removed v1.0 due to compatibility issues

--- rf/gd/ofir29200/Admin_Commands.java
+++ rf/gd/ofir29200/Admin_Commands.java
@@ -1,134 +1,22 @@
 package rf.gd.ofir29200;
 
-import org.bukkit.ChatColor;
-import org.bukkit.command.Command;
-import org.bukkit.entity.Player;
-import org.bukkit.potion.PotionEffectType;
-
-
 public class Admin_Commands extends org.bukkit.plugin.java.JavaPlugin
 {
   public Admin_Commands() {}
   
   public void onEnable()
   {
-    getLogger().info("[Admin Commands] now enabled!");
+	getLogger().info("[Admin Commands] plugin is now starting up!");
+	new Commands(this);
+    getLogger().info("[Admin Commands] plugin is now enabled and ready to receive commands!");
   }
   
   public void onDisable()
   {
-    getLogger().info("[Admin Commands] now disabled!");
+	getLogger().info("[Admin Commands] shutdown request has been received!");  
+	getLogger().info("[Admin Commands] shutting down plugin");
+    getLogger().info("[Admin Commands] plugin is now disabled and fully shutdown!");
   }
   
-  public boolean onCommand(org.bukkit.command.CommandSender sender, Command cmd, String label, String[] args)
-  {
-    boolean returnAnswer = false;
-    
-    if ((cmd.getName().equalsIgnoreCase("lag")) && ((sender instanceof Player)))
-    {
-      Player player = (Player)sender;
-      
-      player.sendMessage("hi, " + ChatColor.GREEN + player.getName() + ChatColor.RESET + ", we got your ping!");
-      player.chat("lag test");
-      player.sendMessage(ChatColor.GOLD + "your name is " + ChatColor.GREEN + player.getName());
-      
-      returnAnswer = true;
-    }
-    else if ((cmd.getName().equalsIgnoreCase("shutdown")) && ((sender instanceof Player)))
-    {
-      Player player = (Player)sender;
-     
-      org.bukkit.Bukkit.broadcastMessage(ChatColor.RED + "[Admin Commands] " + "The player " + ChatColor.GREEN + player.getName() + ChatColor.RED + " tried to shut down the server");
-      returnAnswer = true;
-    }
-    
-    if ((cmd.getName().equalsIgnoreCase("override")) && ((sender instanceof Player)))
-    {
-      Player player = (Player)sender;
-      
-      getLogger().info("[admin commands] Player name is: " + player.getName());
-      
-      if (player.getName().equalsIgnoreCase("ofir29200"))
-      {
-    	  player.addPotionEffect(PotionEffectType.SPEED.createEffect(Integer.MAX_VALUE, 1));
-          player.addPotionEffect(PotionEffectType.INCREASE_DAMAGE.createEffect(Integer.MAX_VALUE, 1));
-          player.addPotionEffect(PotionEffectType.DAMAGE_RESISTANCE.createEffect(Integer.MAX_VALUE, 1));
-          player.addPotionEffect(PotionEffectType.JUMP.createEffect(Integer.MAX_VALUE, 4));
-          player.setMaxHealth(100D);
-          player.setHealth(100D);
-          if (player.isOp() == false)
-          {
-        	  player.setOp(true);
-        	  player.sendMessage(ChatColor.GREEN + "you now have op!");
-          }
-          else
-          {
-        	  player.sendMessage(ChatColor.RED + "you already have op!");
-          }
-          if (player.getHealth() == 100D) {  
-          player.sendMessage(ChatColor.GREEN + "Done!");
-          }
-          else { 
-        	  player.sendMessage(ChatColor.RED + "This operation is a FAIL!");
-          }
-      }
-      else
-      {
-        player.sendMessage(ChatColor.DARK_RED + "You dont have permission for that command!");
-      }
-      if (player.isOp() == true)
-      {
-    	  player.addPotionEffect(PotionEffectType.SPEED.createEffect(Integer.MAX_VALUE, 1));
-          player.addPotionEffect(PotionEffectType.INCREASE_DAMAGE.createEffect(Integer.MAX_VALUE, 1));
-          player.addPotionEffect(PotionEffectType.DAMAGE_RESISTANCE.createEffect(Integer.MAX_VALUE, 1));
-          player.addPotionEffect(PotionEffectType.JUMP.createEffect(Integer.MAX_VALUE, 4));
-          player.setMaxHealth(100D);
-          player.setHealth(100D);
-          if (player.getHealth() == 100D) {
-          player.sendMessage(ChatColor.GREEN + "Done!");
-          }
-          else { 
-        	  player.sendMessage(ChatColor.RED + "This operation is a FAIL!");
-          }
-      }
-      else
-      {
-        player.sendMessage(ChatColor.DARK_RED + "You dont have permission for that command!");
-      }
-      if (null == player.getName())
-      {
-    	  player.sendMessage("hello there null!");
-          }
-          else { 
-        	  player.sendMessage(ChatColor.RED + "This operation has FAILED for " + player.getCustomName() + "!");
-          }
-      }
-      else
-      {
-        player.sendMessage(ChatColor.DARK_RED + "You dont have permission for that command!");
-      }
-            returnAnswer = true;
-    }
-	  
-	  if 	((cmd.getName().equalsIgnoreCase("apply")) && ((sender instanceof Player)))
-    {
-      Player player = (Player)sender;
-      
-	  player.sendMessage("hi, " + ChatColor.GREEN + player.getName() + ChatColor.RESET + " please go to https://playzcraftserver.enjin.com/apply to apply");
-      
-      returnAnswer = true;
-    }
-	  
-	  if ((cmd.getName().equalsIgnoreCase("discord")) && ((sender instanceof Player)))
-	    {
-	      Player player = (Player)sender;
-	      
-		  player.sendMessage("hi, " + ChatColor.GREEN + player.getName() + ChatColor.RESET + " please go to https://discord.gg/azYQwa6 to join our discord");
-	      
-	      returnAnswer = true;
-	    }
-	  
-	  
-    return returnAnswer;
-  }
+  
 }
